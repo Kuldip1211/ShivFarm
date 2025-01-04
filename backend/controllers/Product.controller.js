93a4c57e112b17e9861a36111a4ba9fd51f7e170
Kuldip <kuldeepchudasama6999@gmail.com>
2025-01-05 01:42:54 +0530

    7.25

--- backend/controllers/Product.controller.js
+++ backend/controllers/Product.controller.js
@@ -14,12 +14,9 @@ const addProduct = async (req, res) => {
     } = req.body;
 
     const image1 = req.files.image1 && req.files.image1[0];
-    const image2 = req.files.image2 && req.files.image2[0];
-    const image3 = req.files.image3 && req.files.image3[0];
-    const image4 = req.files.image4 && req.files.image4[0];
 
     // uploading images to cloudinary
-    const images = [image1, image2, image3, image4].filter(
+    const images = [image1].filter(
       (item) => item !== undefined
     );
 
@@ -32,10 +29,10 @@ const addProduct = async (req, res) => {
       })
     );
 
-    /*
-    console.log(name, description, price, category, subCategory, sizes, bestseller);
-    console.log(imagesUrl);
-    */
+
+    // console.log(name, description, price, category, subCategory, sizes, bestseller);
+    // console.log(imagesUrl);
+  
 
     const productData = {
       name,
